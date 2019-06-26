5dc940e3ba66aca66f2966a3c26c31afc8361c36
Yifan Liu <yifan@office-1548mac-6.local>
2019-06-26 10:33:53 -0700

    Initial Commit

---
+++ 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  1
+//
+//  Created by Yifan Liu on 6/26/19.
+//  Copyright © 2019 Yifan Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
